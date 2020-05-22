f41132cfa3369517b1694cdb562ffdf3f95a4bfd
wilson597 <wilkin850@settour.com.tw>
2020-05-22 15:16:58 +0800

    Updates

---
+++ precache-manifest.07a2d167e8a9318de9f99172d6282642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d27fac657defe22611fe62575a3463b",
+    "url": "/WilsonLawler/githubPortfolioPWA/index.html"
+  },
+  {
+    "revision": "50ae94c19fb532268f8a",
+    "url": "/WilsonLawler/githubPortfolioPWA/static/js/2.18b6d339.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WilsonLawler/githubPortfolioPWA/static/js/2.18b6d339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080755293d3e7513a475",
+    "url": "/WilsonLawler/githubPortfolioPWA/static/js/main.7a422d7c.chunk.js"
+  },
+  {
+    "revision": "6fc713431547a275b8ee",
+    "url": "/WilsonLawler/githubPortfolioPWA/static/js/runtime-main.54217288.js"
+  },
+  {
+    "revision": "da95730469727d6df7fb9df78e668784",
+    "url": "/WilsonLawler/githubPortfolioPWA/static/media/gitHubLogo.da957304.png"
+  }
+]);
